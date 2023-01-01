3bd21cef344fdc9881e1cd9042199fe22d7cfe99
Shashi Sharma <sharmashashi940@gmail.com>
2023-01-01 21:42:40 +0545

    added async middleware

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const users = require("./routes/users");
 const auth = require("./routes/auth");
+const error = require("./middleware/error");
 require("./startup/db")();
 const express = require("express");
 const app = express();
@@ -7,6 +8,7 @@ const app = express();
 app.use(express.json());
 app.use("/api/users", users);
 app.use("/api/auth", auth);
+app.use(error);
 
 const port = 8000;
 app.listen(port, () => {
