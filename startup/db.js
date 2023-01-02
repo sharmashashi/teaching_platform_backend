c0b91b101e7d88ac5557d7a7c33df7206bab52fa
Shashi Sharma <sharmashashi940@gmail.com>
2023-01-02 13:23:17 +0545

    added missing imports

--- startup/db.js
+++ startup/db.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
+const winston = require("winston");
 mongoose.set('strictQuery', true);
 module.exports = function () {
   mongoose.connect("mongodb://localhost/hamroguru", (result) => {
-    console.log("Connected to mongo db");
+    winston.info("Connected to mongo db");
   });
 };
